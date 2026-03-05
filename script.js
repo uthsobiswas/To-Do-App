f5c7258db74e2c49ad0456492fe21b4a2fff240a
Uthso Biswas <uthsoroy2002@gmail.com>
2026-03-05 06:15:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,74 +1,26 @@
-const taskInput = document.getElementById("taskInput")
-const taskList = document.getElementById("taskList")
-
-loadTasks()
-
 function addTask(){
 
-const taskText = taskInput.value.trim()
+const input = document.getElementById("taskInput")
+const taskText = input.value
 
-if(taskText === "") return
-
-createTask(taskText)
-
-saveTask(taskText)
-
-taskInput.value = ""
-
+if(taskText === ""){
+alert("Please enter a task")
+return
 }
-
-function createTask(text, completed=false){
 
 const li = document.createElement("li")
 
-if(completed) li.classList.add("completed")
-
 li.innerHTML = `
-<span>${text}</span>
-<button class="delete">X</button>
+${taskText}
+<button class="delete">Delete</button>
 `
 
-li.querySelector("span").onclick = () =>{
-li.classList.toggle("completed")
-updateStorage()
-}
+document.getElementById("taskList").appendChild(li)
 
-li.querySelector(".delete").onclick = () =>{
+input.value = ""
+
+li.querySelector(".delete").onclick = function(){
 li.remove()
-updateStorage()
 }
-
-taskList.appendChild(li)
-
-}
-
-function saveTask(task){
-let tasks = JSON.parse(localStorage.getItem("tasks")) || []
-tasks.push({text:task,completed:false})
-localStorage.setItem("tasks",JSON.stringify(tasks))
-}
-
-function loadTasks(){
-let tasks = JSON.parse(localStorage.getItem("tasks")) || []
-
-tasks.forEach(task =>{
-createTask(task.text,task.completed)
-})
-}
-
-function updateStorage(){
-
-let tasks = []
-
-document.querySelectorAll("#taskList li").forEach(li =>{
-
-tasks.push({
-text:li.innerText.replace("X","").trim(),
-completed:li.classList.contains("completed")
-})
-
-})
-
-localStorage.setItem("tasks",JSON.stringify(tasks))
 
 }
